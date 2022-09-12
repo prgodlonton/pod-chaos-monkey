a27bd8de9af43bdc5c211e6fea38b3fe9ea73c46
Patrick Godlonton <patrick.godlonton@lush.co.uk>
2022-09-12 22:46:45 +0100

    Refactored tests.

--- cli/internal/commands/disruptor_test.go
+++ cli/internal/commands/disruptor_test.go
@@ -11,6 +11,10 @@ import (
 	"time"
 )
 
+const (
+	testTimeout = time.Second
+)
+
 type MockDeleter struct {
 	Callback  func(ctx context.Context, name string) error
 	WasCalled bool
@@ -46,106 +50,132 @@ type MockDeleterLister struct {
 	MockLister
 }
 
-func TestPodDeleterReturnsImmediatelyWhenPassedCanceledContext(t *testing.T) {
-	deleter, err := commands.NewPodDisruptor(&MockDeleterLister{}, time.Duration(0))
-	if err != nil {
-		t.Fatalf("cannot create pod deleter: %v", err)
-	}
-
-	ctx, cfn := context.WithCancel(context.Background())
-	cfn()
-
-	assert.Nil(t, deleter.Disrupt(ctx, ""))
-}
-
-func TestPodDeleterCallsListerWithSelector(t *testing.T) {
+func TestPodDisruptor(t *testing.T) {
 	as := assert.New(t)
-	expectedSelector := "app=nginx"
 
 	ctx, cfn := context.WithCancel(context.Background())
 	defer cfn()
 
 	dl := &MockDeleterLister{
-		MockLister: MockLister{
-			Callback: func(_ context.Context, actualSelector string) ([]string, error) {
+		MockDeleter: MockDeleter{
+			Callback: func(_ context.Context, name string) error {
 				defer func() {
 					cfn()
 				}()
-				as.Equal(expectedSelector, actualSelector)
-				return []string{}, nil
-			},
-		},
-	}
-	deleter, err := commands.NewPodDisruptor(dl, time.Duration(0))
-	if err != nil {
-		t.Fatalf("cannot create pod deleter: %v", err)
-	}
-
-	as.Nil(deleter.Disrupt(ctx, expectedSelector))
-	as.True(dl.MockLister.WasCalled)
-}
-
-func TestPodDeleterReturnsErrorWhenListerReturnsError(t *testing.T) {
-	dl := &MockDeleterLister{
-		MockLister: MockLister{
-			Callback: func(_ context.Context, actualSelector string) ([]string, error) {
-				return []string{}, errors.New("intentional error")
-			},
-		},
-	}
-	deleter, err := commands.NewPodDisruptor(dl, time.Duration(0))
-	if err != nil {
-		t.Fatalf("cannot create pod deleter: %v", err)
-	}
-
-	as := assert.New(t)
-	as.NotNil(deleter.Disrupt(context.Background(), ""))
-	as.True(dl.MockLister.WasCalled)
-}
-
-func TestPodDeleterDoesNothingWhenListerReturnsEmptyList(t *testing.T) {
-	calls := make(chan struct{})
-	dl := &MockDeleterLister{
-		MockDeleter: MockDeleter{
-			Callback: func(_ context.Context, _ string) error {
-				t.Fatalf("delete unexpectedly called")
+				as.Equal("pod1", name)
 				return nil
 			},
 		},
 		MockLister: MockLister{
-			Callback: func(_ context.Context, actualSelector string) ([]string, error) {
-				defer func() {
-					calls <- struct{}{}
-				}()
-				return []string{}, nil
+			Callback: func(_ context.Context, _ string) ([]string, error) {
+				return []string{
+					"pod1",
+				}, nil
 			},
 		},
 	}
-	deleter, err := commands.NewPodDisruptor(dl, time.Duration(0))
+	disruptor, err := commands.NewPodDisruptor(dl, time.Duration(0))
 	if err != nil {
-		t.Fatalf("cannot create pod deleter: %v", err)
+		t.Fatalf("cannot create disruptor: %v", err)
 	}
+
+	as.Nil(disruptor.Disrupt(ctx, ""))
+}
+
+func TestPodDisruptorPassesSelectorToLister(t *testing.T) {
+	as := assert.New(t)
 
 	ctx, cfn := context.WithCancel(context.Background())
 	defer cfn()
 
-	go func() {
-		assert.Nil(t, deleter.Disrupt(ctx, ""))
-	}()
-
-	for i := 0; i < 10; {
-		<-calls
-		i++
+	dl := &MockDeleterLister{
+		MockLister: MockLister{
+			Callback: func(_ context.Context, selector string) ([]string, error) {
+				defer func() {
+					cfn()
+				}()
+				as.Equal("field1=value1", selector)
+				return []string{}, nil
+			},
+		},
 	}
-	close(calls)
+	disruptor, err := commands.NewPodDisruptor(dl, time.Duration(0))
+	if err != nil {
+		t.Fatalf("cannot create disruptor: %v", err)
+	}
+
+	as.Nil(disruptor.Disrupt(ctx, "field1=value1"))
+	as.True(dl.MockLister.WasCalled)
 }
 
-func TestPodDeleterReturnsErrorWhenDeleterReturnsError(t *testing.T) {
+func TestPodDisruptorDoesNothingWhenPodListIsEmpty(t *testing.T) {
 	as := assert.New(t)
+
+	ctx, cfn := context.WithTimeout(context.Background(), testTimeout)
+	defer cfn()
+
+	dl := &MockDeleterLister{
+		MockDeleter: MockDeleter{
+			Callback: func(_ context.Context, name string) error {
+				t.Fatalf("pod delete unexpectedly called")
+				return nil
+			},
+		},
+		MockLister: MockLister{
+			Callback: func(_ context.Context, _ string) ([]string, error) {
+				return []string{}, nil
+			},
+		},
+	}
+	disruptor, err := commands.NewPodDisruptor(dl, time.Duration(0))
+	if err != nil {
+		t.Fatalf("cannot create disruptor: %v", err)
+	}
+
+	as.Nil(disruptor.Disrupt(ctx, ""))
+	as.False(dl.MockDeleter.WasCalled)
+	as.True(dl.MockLister.WasCalled)
+}
+
+func TestPodDisruptorReturnsWhenPassedCanceledContext(t *testing.T) {
+	ctx, cfn := context.WithTimeout(context.Background(), testTimeout)
+	cfn()
+
+	disruptor, err := commands.NewPodDisruptor(&MockDeleterLister{}, time.Duration(0))
+	if err != nil {
+		t.Fatalf("cannot create disruptor: %v", err)
+	}
+
+	assert.Nil(t, disruptor.Disrupt(ctx, ""))
+}
+
+func TestPodDisruptorReturnsErrorWhenListerReturnsError(t *testing.T) {
+	expectedErr := errors.New("intentional error")
+	dl := &MockDeleterLister{
+		MockLister: MockLister{
+			Callback: func(_ context.Context, _ string) ([]string, error) {
+				return []string{}, expectedErr
+			},
+		},
+	}
+	disruptor, err := commands.NewPodDisruptor(dl, time.Duration(0))
+	if err != nil {
+		t.Fatalf("cannot create disruptor: %v", err)
+	}
+
+	err = disruptor.Disrupt(context.Background(), "")
+
+	as := assert.New(t)
+	as.ErrorIs(err, expectedErr)
+	as.True(dl.MockLister.WasCalled)
+}
+
+func TestPodDisruptorReturnsErrorWhenDeleterReturnsError(t *testing.T) {
+	expectedErr := errors.New("intentional error")
 	dl := &MockDeleterLister{
 		MockDeleter: MockDeleter{
 			Callback: func(_ context.Context, _ string) error {
-				return errors.New("intentional error")
+				return expectedErr
 			},
 		},
 
@@ -157,47 +187,15 @@ func TestPodDeleterReturnsErrorWhenDeleterReturnsError(t *testing.T) {
 			},
 		},
 	}
-	deleter, err := commands.NewPodDisruptor(dl, time.Duration(0))
+	disruptor, err := commands.NewPodDisruptor(dl, time.Duration(0))
 	if err != nil {
-		t.Fatalf("cannot create pod deleter: %v", err)
+		t.Fatalf("cannot create disruptor: %v", err)
 	}
 
-	as.NotNil(deleter.Disrupt(context.Background(), ""))
-	as.True(dl.MockDeleter.WasCalled)
-}
+	err = disruptor.Disrupt(context.Background(), "")
 
-func TestPodDeleterSelectsPodForDeletionFromList(t *testing.T) {
 	as := assert.New(t)
-
-	ctx, cfn := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cfn()
-
-	pods := []string{
-		"pod1",
-		"pod2",
-	}
-	dl := &MockDeleterLister{
-		MockDeleter: MockDeleter{
-			Callback: func(_ context.Context, name string) error {
-				defer func() {
-					cfn()
-				}()
-				as.Contains(pods, name)
-				return nil
-			},
-		},
-
-		MockLister: MockLister{
-			Callback: func(_ context.Context, actualSelector string) ([]string, error) {
-				return pods, nil
-			},
-		},
-	}
-	deleter, err := commands.NewPodDisruptor(dl, time.Duration(0))
-	if err != nil {
-		t.Fatalf("cannot create pod deleter: %v", err)
-	}
-
-	as.Nil(deleter.Disrupt(ctx, ""))
+	as.ErrorIs(err, expectedErr)
 	as.True(dl.MockDeleter.WasCalled)
+	as.True(dl.MockLister.WasCalled)
 }
